e3cddbce7baf54d5c71ed0e683cf6a5a6c03de7d
jonhatanh <jonhatan.higuera@gmail.com>
2024-01-23 14:35:44 -0600

    Add images and main menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'
 
 function App () {
   return (
     <>
-      <div className='bg-green-400 min-h-dvh'>
-        <h1 className='flex bg-red-50'>Memory Card Game</h1>
+      <div className='min-h-dvh bg-black'>
+        {/* Main Menu */}
+        <div className='relative mx-auto flex min-h-dvh w-full items-center justify-center border-2 border-white'>
+          <img
+            className='absolute h-full w-full object-cover object-bottom opacity-60'
+            src='/src/assets/forestBackground.png'
+            alt='Main Menu Image'
+          />
+          <div className='relative w-80 rounded-md border-b-2 border-t-2 border-dashed border-white border-opacity-30 p-2'>
+            <h1 className='flex flex-col text-center text-6xl text-slate-700'>
+              <span className='drop-shadow-surrounded-blue text-[#FFCC00]'>
+                Pokémon
+              </span>
+              <span className='drop-shadow-surrounded'>Memory Card</span>
+            </h1>
+            <div className='m-5 ml-8'>
+              <button className='group text-2xl text-white flex items-center'>
+                <FontAwesomeIcon
+                  className='opacity-0 mb-1 duration-150 group-hover:opacity-100'
+                  icon={faCaretRight}
+                />
+                <span className='block text-center duration-150 transition-all ease-out group-hover:translate-x-2'>
+                  Start Game
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'main-menu': "url('/src/assets/forestBackground.png')"
+      },
+      dropShadow: {
+        surrounded: [
+          '2px 2px  white',
+          '-2px -2px white',
+          '0 2px white',
+          '0 -2px white'
+        ],
+        'surrounded-blue': [
+          '2px 2px  #0075BE',
+          '-2px -2px #0075BE',
+          '0 2px #0075BE',
+          '0 -2px #0075BE'
+        ],
+      }
+    }
   },
   plugins: []
 }
